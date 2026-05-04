9b3195789446fae2a8c8f6158e3aba0559e39f38
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-05-04 21:29:37 +0530

    type in js

--- Project/JS/index.js
+++ Project/JS/index.js
@@ -17,7 +17,37 @@ var c;
 
 
 
-   // condtional 
+                                                    // type in js 
+
+// premitive type
+
+console.log("premitive type")
+ var a = 2;
+  var b =a;
+   console.log(a)
+   console.log(b);
+   a+= 2;
+   console.log(a)
+   console.log(b);
+   console.log(a)
+   
+   
+
+   // reference type 
+console.log("refrence type")
+   var x=[1,2,3,4,5]
+   var y = x;
+   console.log(x);
+   console.log(y);
+
+   x.pop()
+   console.log(y);
+   console.log(x);
+   console.log(y);
+   
+
+
+                                                 // condtional 
    if(-1){
     console.log("not true")
    }
