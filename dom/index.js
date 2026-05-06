e276e0bbe96d07209b881098ac1f9fd5a11e2b80
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-05-06 22:44:02 +0530

    addevent

--- dom/index.js
+++ dom/index.js
@@ -1,5 +1,39 @@
-var a  = document.querySelector("h1")
+// var a  = document.querySelector("h1")
 // window.alert(a)
-a.innerHTML = "badal barsha bijli sawan ka pani "
+// a.innerHTML = `badal barsha bijli sawan ka pani<br>
+// adat pay jyu hasna de tanu`
 
-a.style ="color: blue"
+
+
+// // a.style ="color: blue"
+// a.style.color = "blue";
+// a.style.backgroundColor = "black"
+// // a.style = "background-color: red"
+
+// a.addEventListener("click", function(){
+//     console.log("hey")
+// })
+
+
+var blub = document.querySelector("div")
+var btn  = document.querySelector("button")
+var flag = 0
+
+
+btn.addEventListener("click", function(){
+    if(flag == 0){
+    blub.style.backgroundColor = "yellow"
+    btn.innerHTML ="of"
+    flag =1
+    }
+    else{
+    blub.style.backgroundColor = "white"
+    btn.innerHTML ="on"
+    flag = 0
+
+    }
+
+
+})
+
+
